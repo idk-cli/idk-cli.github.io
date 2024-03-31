6c077f2afeaebe297377bd5e345cdafb15907c67
rishi <rishijash@hotmail.com>
2024-03-30 22:42:08 -0700

    Website UI updates

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
 function redirectToURL(url) {
     window.location.href = url;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+
+    menuToggle.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
+});
