45f3320e505c9b07806f114b8d17987995dd0173
bryanmax9 <brillant16.gcv@gmail.com>
2024-05-05 10:36:37 -0700

    website finished and pushed👻

--- scripts/install.sh
+++ scripts/install.sh
@@ -64,6 +64,10 @@ unzip -o "$TEMP_DIR/$TEMP_FILE_NAME" -d "$TEMP_DIR" >/dev/null 2>&1
 # Determine the executable name
 EXECUTABLE_NAME=$(determine_executable_name)
 
+# Inform the user about the need for sudo access
+echo "Requesting administrative access to install IDK to $INSTALL_DIR."
+echo "This access is required to move and set executable permissions for the IDK binary."
+
 # Move the appropriate executable to /usr/local/bin (requires sudo)
 sudo chmod +x "$TEMP_DIR/idk_terminal-$SCRIPT_LATEST_VERSION/binaries/$EXECUTABLE_NAME"
 sudo mv "$TEMP_DIR/idk_terminal-$SCRIPT_LATEST_VERSION/binaries/$EXECUTABLE_NAME" "$INSTALL_DIR/$INSTALL_FILE"
